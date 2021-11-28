8893b65bca4c86f99e0a1f4a2ccbc73abfc6dedc
jeremyacoop <63517273+jeremyacoop@users.noreply.github.com>
2021-11-28 15:46:44 -0600

    Wrote addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,13 @@
 console.log('***** Music Collection *****')
+
+const collection = [];
+
+function addToCollection(title, artist, yearPublished) {
+    const musicAlbum = {
+        title: title,
+        artist: artist, 
+        yearPublished: yearPublished
+    }
+    collection.push(musicAlbum);
+    return musicAlbum;
+}// end addToCollection
